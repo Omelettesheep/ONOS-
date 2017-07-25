f96959025f99eef03be71136287e7f5cf66fa9ba
杨一 <yangyi7@yiche.com>
2017-07-25 18:10:07 +0800

    圆圈拖拽

---
+++ js/onos-network.js
@@ -0,0 +1,37 @@
+// // var onosNetWork = function(){};
+// var graph = new joint.dia.Graph;
+// var paper = new joint.dia.Paper({ 
+//     el: $('.order'), 
+//     width: '100%', 
+//     gridSize:10 , 
+//     model: graph,
+//     // linkPinning:true,
+//     // interactive:true,
+//     drawGrid: 'mesh'
+
+// });
+// var graph = new joint.dia.Graph;
+// var paper = new joint.dia.Paper({ 
+//     el: $('.order'), 
+//     width: '100%', 
+//     gridSize:10 , 
+//     model: this.graph,
+//     drawGrid: 'mesh'
+// });
+
+// onosNetWork.prototype = {
+//     graph:'',
+//     paper:'',
+//     result:{
+//         node:[],
+//         connection:[]
+//     },
+//     createPaper:function(){
+        
+
+
+//     }
+// }
+
+// var onosNetWork = new onosNetWork();
+// console.log(onosNetWork);
